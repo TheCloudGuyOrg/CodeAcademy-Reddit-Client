baa8fea3a6080da2ee50ff7b154cb8115a48af98
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-12-08 14:50:36 -0700

    Building header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import Header from './features/Header/Header'
 import './App.css';
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <main>
-        {/* <Home /> */}
       </main>
       <aside>
-        {/* <Subreddits /> */}
       </aside>
     </>
   );
