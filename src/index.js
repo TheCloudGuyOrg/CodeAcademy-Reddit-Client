5483e4b9b6dc19e86c7be2d1b5afdec9dd81cad1
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-12-08 13:22:01 -0700

    Setting up Store Scaffolding

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './store/store.js';
+import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>,
-    document.getElementById('root')
-  </React.StrictMode>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 
