1c024a0af956b8b77231eb747ebfec69aceecd59
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-12-08 14:50:20 -0700

    Building Reddit Reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import redditReducer from './redditSlice';
 import subRedditReducer from './subRedditSlice';
 
 export default configureStore({
-    reducer: combineReducers({
-        subreddits: subRedditReducer
-    })
+  reducer: combineReducers({
+    reddit: redditReducer,
+    subreddits: subRedditReducer,
+  }),
 });
